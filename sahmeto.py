5787558c9268a2cdb079b7d8451ddd33f291b5e4
Negar <negarad369.gmail.com>
2024-06-29 22:06:15 +0430

    Convert xml data to json

---
+++ sahmeto.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import xmltodict
+
+
+url = "https://sahmeto.com/crypto-sitemap.xml"
+
+
+response = requests.get(url)
+
+
+if response.status_code == 200:
+   
+    xml_data = response.content.decode('utf-8')
+
+    
+    data_dict = xmltodict.parse(xml_data)
+
+    
+    json_data = json.dumps(data_dict, indent=4)
+
+    
+    print(json_data)
+else:
+    print("Error loading xml data:", response.status_code)
